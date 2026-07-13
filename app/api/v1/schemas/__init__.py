dc67b6d2012fd206869177a4092a2fdf18ef89ce
Max L <letuwwa@gmail.com>
2026-07-13 17:06:43 +0300

    Initial commit

---
+++ app/api/v1/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .token import AccessToken, AuthResponse, TokenPair
+from .user import UserRead, UserRegister
+
+
+__all__ = ["AccessToken", "AuthResponse", "TokenPair", "UserRead", "UserRegister"]
---
+++ app/db/models/__init__.py
@@ -0,0 +1,4 @@
+from app.db.models.token_blocklist import TokenBlocklist
+from app.db.models.user import User, UserRole
+
+__all__ = ["TokenBlocklist", "User", "UserRole"]
