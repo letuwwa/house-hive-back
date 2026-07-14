20b573bed5c7ef1d3f8595000ad0dd14414b50c7
Max Litvin <letuwwa@gmail.com>
2026-07-14 12:11:43 +0300

    feat: House, HouseMember and Event models added

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,4 +1,15 @@
+from app.db.models.event import Event
+from app.db.models.house import House
+from app.db.models.house_member import HouseMember, HouseMemberRole
 from app.db.models.token_blocklist import TokenBlocklist
 from app.db.models.user import User, UserRole
 
-__all__ = ["TokenBlocklist", "User", "UserRole"]
+__all__ = [
+    "Event",
+    "House",
+    "HouseMember",
+    "HouseMemberRole",
+    "User",
+    "UserRole",
+    "TokenBlocklist",
+]
